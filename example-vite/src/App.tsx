419e9bd512acd34bcbe40a4870fea9d6797f1c32
tonyedgal <tonyedgal@gmail.com>
2025-08-09 12:49:20 +0100

    feat: setup vite, react, shadcn and spaceman RTA package

---
+++ example-vite/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-background text-foreground">
+      <h1 className="text-4xl font-bold">Welcome to Spaceman RTA Framework!</h1>
+    </div>
+  );
+}
+
+export default App;
